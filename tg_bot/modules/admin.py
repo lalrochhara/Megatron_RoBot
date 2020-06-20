12dad9deba1606ec0ef53e40a0d92165ddfd9603
Gogeta~Sama <55589931+gogeta0sama@users.noreply.github.com>
2020-06-20 18:39:43 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -11,6 +11,7 @@ from tg_bot import dispatcher, TOKEN
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin, connection_status
 from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
+from tg_bot.modules.tr_engine.strings import tld
 from tg_bot.modules.log_channel import loggable
 
 
